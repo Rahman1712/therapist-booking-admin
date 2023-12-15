2fdf876eaebbc7f91ba24d3f146a6930da0fe1eb
abdul rahman <abdulrahman1712@gmail.com>
2023-12-15 17:20:05 +0530

    change made env file

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,14 +1,26 @@
 export const environment = {
   production: true ,
-  apiBaseUrl: 'http://localhost:8989/admins/api/v1/auth',
-  apiAdminPrivateUrl: 'http://localhost:8989/admins/api/v1/private',
-  apiAdminUsersUrl: 'http://localhost:8989/admins/api/v1/users',
-  apiAdminTherapistsUrl: 'http://localhost:8989/admins/api/v1/therapists',
-  apiAdminCategoryUrl: 'http://localhost:8989/admins/api/v1/category',
-  apiTherapistsUrl: 'http://localhost:8989/therapists/api/v1/public',
-  apiTherapistsAdminPublicUrl: 'http://localhost:8989/therapists/api/v1/public-to-admin', // Direct From Therapists
+  apiBaseUrl: 'https://medicarehealth.website:8989/admins/api/v1/auth',
+  apiAdminPrivateUrl: 'https://medicarehealth.website:8989/admins/api/v1/private',
+  apiAdminUsersUrl: 'https://medicarehealth.website:8989/admins/api/v1/users',
+  apiAdminTherapistsUrl: 'https://medicarehealth.website:8989/admins/api/v1/therapists',
+  apiAdminCategoryUrl: 'https://medicarehealth.website:8989/admins/api/v1/category',
+  apiTherapistsUrl: 'https://medicarehealth.website:8989/therapists/api/v1/public',
+  apiTherapistsAdminPublicUrl: 'https://medicarehealth.website:8989/therapists/api/v1/public-to-admin', // Direct From Therapists
   appName: 'MediCare 💙',
 };
+
+// export const environment = {
+//   production: true ,
+//   apiBaseUrl: 'http://localhost:8989/admins/api/v1/auth',
+//   apiAdminPrivateUrl: 'http://localhost:8989/admins/api/v1/private',
+//   apiAdminUsersUrl: 'http://localhost:8989/admins/api/v1/users',
+//   apiAdminTherapistsUrl: 'http://localhost:8989/admins/api/v1/therapists',
+//   apiAdminCategoryUrl: 'http://localhost:8989/admins/api/v1/category',
+//   apiTherapistsUrl: 'http://localhost:8989/therapists/api/v1/public',
+//   apiTherapistsAdminPublicUrl: 'http://localhost:8989/therapists/api/v1/public-to-admin', // Direct From Therapists
+//   appName: 'MediCare 💙',
+// };
 
 
 /*
